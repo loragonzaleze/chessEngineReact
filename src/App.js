15b4af08fe64738b62b3c48ade0b91ed2282e1ec
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2021-06-27 00:40:28 -0400

    First change, should display a new message

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>TESTING IF GIT IS WORKING CORRECTLY</h1>
         </p>
         <a
           className="App-link"
